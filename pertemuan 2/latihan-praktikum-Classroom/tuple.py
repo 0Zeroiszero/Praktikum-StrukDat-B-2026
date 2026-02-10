9c2a68c8ce8464e76b8a5333218beedb117e11f1
0Zeroiszero <abdullahaffandi0109@gmail.com>
2026-02-10 22:29:20 +0700

    finish laporan-praktikum-Classroom

--- pertemuan 2/latihan-praktikum-Classroom/tuple.py
+++ pertemuan 2/latihan-praktikum-Classroom/tuple.py
@@ -11,7 +11,7 @@ index_result1 = sample_tuple.index(2)
 index_result2 = sample_tuple.index(2, 2)
 index_result3 = sample_tuple.index(2, 4, 7)              
 
-print("Tuple methods demo dengan parameter tambahan:")
+print("Hasil implementasi method tuple:")
 print(f"Tuple: {sample_tuple}")
 print(f"count(2): {count_result}")
 print(f"index(2): {index_result1}")
